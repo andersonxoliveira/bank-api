41f11537453ed6be056468bc73b3d550fe38fa30
Anderson Oliveira <aondersonoliveira@gmail.com>
2022-07-24 19:17:28 -0300

    add user_type to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_one :account
+  before_validation :set_user_type
+
+  enum user_type: {
+    admin: 0,
+    normal: 1,
+    dba: 3
+  }, _prefix: :user_type
+
+  private
+
+  def set_user_type
+    self.user_type = :normal
+  end
 end
